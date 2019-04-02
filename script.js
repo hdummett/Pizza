39eabe4833b90faaafcc58e92a1883d4150dd07c
MollyGoodyer <47502016+MollyGoodyer@users.noreply.github.com>
2019-04-02 11:21:42 +0100

    button update +social media tag attempt that didnt work

--- script.js
+++ script.js
@@ -23,3 +23,8 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+$(window).scroll(function(){
+ var navTop =  $(window).scrollTop();
+ $('.model-0').css("top", navTop + 50);
+});
