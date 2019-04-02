bbfb4282def35ca6761368ff8e893138799ffb68
MollyGoodyer <47502016+MollyGoodyer@users.noreply.github.com>
2019-04-02 18:49:52 +0100

    trying to get the bubble back

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ var btn = document.getElementByClassName("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+// clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
